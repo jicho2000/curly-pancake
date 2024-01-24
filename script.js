fb431c302842bc76e2f30db44e3fa8851a3e75eb
jicho2000 <levani.jichonaia2@gmail.com>
2024-01-24 20:38:39 +0400

    some changes on footer

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
+// header moving js
+document.addEventListener("DOMContentLoaded", function () {
+    var header = document.querySelector('.header');
+    window.addEventListener('scroll', function () {
+    var scrollPosition = window.scrollY;
+        if (scrollPosition === 0) {
+        header.style.backgroundColor = 'rgba(26, 30, 31, 1)';
+        } else {
+        header.style.backgroundColor = 'rgba(26, 30, 31, 0.9)';
+        }
+    });
+    });
+// carousel js
 let currentSlide = 1;
 
 function showSlide(slideIndex) {
-  const slides = document.querySelectorAll('.slider__logos > div');
-  const dots = document.querySelectorAll('.slider__btn__ol a');
+const slides = document.querySelectorAll('.slider__logos > div');
+const dots = document.querySelectorAll('.slider__btn__ol a');
 
-  if (slideIndex > slides.length) {
-      currentSlide = 1;
-  } else if (slideIndex < 1) {
-      currentSlide = slides.length;
-  } else {
-      currentSlide = slideIndex;
-  }
+if (slideIndex > slides.length) {
+    currentSlide = 1;
+} else if (slideIndex < 1) {
+    currentSlide = slides.length;
+} else {
+    currentSlide = slideIndex;
+}
 
-  slides.forEach((slide, index) => {
-      slide.style.display = index + 1 === currentSlide ? 'flex' : 'none';
-  });
+slides.forEach((slide, index) => {
+    slide.style.display = index + 1 === currentSlide ? 'flex' : 'none';
+});
 
-  dots.forEach((dot, index) => {
-      dot.classList.toggle('active', index + 1 === currentSlide);
-  });
+dots.forEach((dot, index) => {
+    dot.classList.toggle('active', index + 1 === currentSlide);
+});
 }
 
 function nextSlide() {
